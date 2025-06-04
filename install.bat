18f511cccc8a2ecdc6c4579b18eb298abb5be851
managanemeke <managanemeke@gmail.com>
2025-06-04 19:04:26 +0500

    feat(install): create script

--- install.bat
+++ install.bat
@@ -4,12 +4,12 @@ CHCP 65001
 SET path=%~dp0
 
 SET "name=Сжать"
-SET "type=*"
+SET "type=.png"
 SET "position=Bottom"
 SET command=%path%file.bat ""%%1""
 
-reg add "HKCR\%type%\shell\%name%" /f
-reg add "HKCR\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
-reg add "HKCR\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
-reg add "HKCR\%type%\shell\%name%\command" /f
-reg add "HKCR\%type%\shell\%name%\command" /f /ve /d "%command%"
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f /ve /d "%command%"
