87aefed27d87010f1b9902b7ca266d1d2a3da3fa
managanemeke <managanemeke@gmail.com>
2025-06-11 05:42:27 +0500

    feat(install_folder): fill with logic

---
+++ install_folder.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+CHCP 65001
+
+SET "path=%~dp0"
+SET "name=Сжать"
+SET "position=Bottom"
+SET command=%path%folder.bat ""%%1""
+
+CALL :add_context_menu_item_for_extension "Directory.Image"
+
+EXIT /B %ERRORLEVEL%
+
+:add_context_menu_item_for_extension
+SET "type=%~1"
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f
+reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f /ve /d "%command%"
+EXIT /B 0
