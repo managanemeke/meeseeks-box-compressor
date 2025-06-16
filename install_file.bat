0a6eb4e1b75e1e0249b73f69264c64629f432f3a
managanemeke <managanemeke@gmail.com>
2025-06-16 16:26:01 +0500

    feat(install_file): use * instead .png, .jpg and .jpeg to escape from duplicate in regedit

--- install_file.bat
+++ install_file.bat
@@ -6,17 +6,15 @@ SET "name=Сжать"
 SET "position=Bottom"
 SET command=%path%file.bat ""%%1""
 
-CALL :add_context_menu_item_for_type ".png"
-CALL :add_context_menu_item_for_type ".jpg"
-CALL :add_context_menu_item_for_type ".jpeg"
+CALL :add_context_menu_item_for_type "*"
 
 EXIT /B %ERRORLEVEL%
 
 :add_context_menu_item_for_type
 SET "type=%~1"
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f /ve /d "%command%"
+reg add "HKCR\%type%\shell\%name%" /f
+reg add "HKCR\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
+reg add "HKCR\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
+reg add "HKCR\%type%\shell\%name%\command" /f
+reg add "HKCR\%type%\shell\%name%\command" /f /ve /d "%command%"
 EXIT /B 0
