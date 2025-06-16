a150edd72f72ac6978658d117d3763d2456061d0
managanemeke <managanemeke@gmail.com>
2025-06-16 15:36:35 +0500

    fix(install_folder): use global Folder instead Directory.Image

--- install_folder.bat
+++ install_folder.bat
@@ -6,15 +6,15 @@ SET "name=Сжать"
 SET "position=Bottom"
 SET command=%path%folder.bat ""%%1""
 
-CALL :add_context_menu_item_for_type "Directory.Image"
+CALL :add_context_menu_item_for_type "Folder"
 
 EXIT /B %ERRORLEVEL%
 
 :add_context_menu_item_for_type
 SET "type=%~1"
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f
-reg add "HKCR\SystemFileAssociations\%type%\shell\%name%\command" /f /ve /d "%command%"
+reg add "HKCR\%type%\shell\%name%" /f
+reg add "HKCR\%type%\shell\%name%" /f /v MUIVerb /t REG_SZ /d %name%
+reg add "HKCR\%type%\shell\%name%" /f /v Position /t REG_SZ /d %position%
+reg add "HKCR\%type%\shell\%name%\command" /f
+reg add "HKCR\%type%\shell\%name%\command" /f /ve /d "%command%"
 EXIT /B 0
